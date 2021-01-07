649ac90c4d75548db8ac9882f0399a4284d2f0f0
xing zhang <zhangxing@broada.com>
2021-01-07 17:53:11 +0800

    commit

--- src/main/java/nick/leetcode/q100/Q2.java
+++ src/main/java/nick/leetcode/q100/Q2.java
@@ -15,6 +15,60 @@ package nick.leetcode.q100;
  */
 public class Q2 {
 
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = null, tail = null;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int n1 = l1 != null ? l1.val : 0;
+            int n2 = l2 != null ? l2.val : 0;
+            int sum = n1 + n2 + carry;
+            if (head == null) {
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
+            carry = sum / 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry > 0) {
+            tail.next = new ListNode(carry);
+        }
+        return head;
+    }
+
     public static void main(String[] args) {
+        ListNode l1 = new ListNode(2, new ListNode(3, new ListNode(5)));
+        ListNode l2 = new ListNode(1, new ListNode(7, new ListNode(5, new ListNode(1, new ListNode(5)))));
+        ListNode result = addTwoNumbers(l1, l2);
+        while (null != result.next) {
+            System.out.print(result.val);
+            result = result.next;
+            if (null == result.next) {
+                System.out.println(result.val);
+            }
+        }
     }
 }
